4097414fc23f65e22f790a8aa1fb36c43e886d90
Vova Petrov <v.petrov3@g.nsu.ru>
2025-03-30 13:07:24 +0700

    dependency manager refactoring

--- di-container/src/main/java/ru/nsu/context/DependenciesManager.java
+++ di-container/src/main/java/ru/nsu/context/DependenciesManager.java
@@ -24,35 +24,35 @@ public class DependenciesManager {
 
 
     public List<String> resolveDependencies() {
+        List<String> sortedBeans = new ArrayList<>();
         DefaultDirectedGraph<String, DefaultEdge> graph = new DefaultDirectedGraph<>(DefaultEdge.class);
+
         beans.keySet().forEach(graph::addVertex);
         beans.forEach((beanName, beanDefinition) -> addDependenciesToGraph(beanName, beanDefinition, graph));
 
-        CycleDetector<String, DefaultEdge> cycleDetector = new CycleDetector<>(graph);
-        if (cycleDetector.detectCycles()) {
-            throw new RuntimeException("Detected cyclic dependencies");
-        }
+        checkForCycleDependency(graph);
 
         TopologicalOrderIterator<String, DefaultEdge> orderIterator = new TopologicalOrderIterator<>(graph);
-        List<String> sortedBeans = new ArrayList<>();
+
         orderIterator.forEachRemaining(sortedBeans::add);
 
         return sortedBeans;
     }
 
-    private void addDependenciesToGraph(String beanName, BeanObject bean, DefaultDirectedGraph<String, DefaultEdge> graph) {
-        if (bean != null) {
-            var injectedFields = bean.getInjectedFields();
-            if (injectedFields != null) {
-                injectedFields.forEach((field) -> {
-                    Named namedAnnotation = field.getAnnotation(Named.class);
-                    if (namedAnnotation != null && graph.containsVertex(namedAnnotation.value())) {
-                        graph.addEdge(beanName, namedAnnotation.value());
-                    }
-                });
-            }
+    private void checkForCycleDependency(DefaultDirectedGraph<String, DefaultEdge> graph) {
+        CycleDetector<String, DefaultEdge> cycleDetector = new CycleDetector<>(graph);
+        if (cycleDetector.detectCycles()) {
+            throw new RuntimeException("Detected cyclic dependencies");
         }
+    }
 
+    private void addDependenciesToGraph(String beanName, BeanObject bean, DefaultDirectedGraph<String, DefaultEdge> graph) {
+        addInjectedFields(beanName, bean, graph);
+        addInjectProviderFields(beanName, bean, graph);
+        addConstructorParameters(beanName, bean, graph);
+    }
+
+    private void addInjectProviderFields(String beanName, BeanObject bean, DefaultDirectedGraph<String, DefaultEdge> graph) {
         if (bean != null) {
             var injectedProviderFieldsFields = bean.getInjectedProviderFields();
             if (injectedProviderFieldsFields != null) {
@@ -64,7 +64,23 @@ public class DependenciesManager {
                 });
             }
         }
+    }
 
+    private void addInjectedFields(String beanName, BeanObject bean, DefaultDirectedGraph<String, DefaultEdge> graph) {
+        if (bean != null) {
+            var injectedFields = bean.getInjectedFields();
+            if (injectedFields != null) {
+                injectedFields.forEach((field) -> {
+                    Named namedAnnotation = field.getAnnotation(Named.class);
+                    if (namedAnnotation != null && graph.containsVertex(namedAnnotation.value())) {
+                        graph.addEdge(beanName, namedAnnotation.value());
+                    }
+                });
+            }
+        }
+    }
+
+    private void addConstructorParameters(String beanName, BeanObject bean, DefaultDirectedGraph<String, DefaultEdge> graph) {
         Constructor<?> constructor = bean.getConstructor();
         if (constructor != null && constructor.isAnnotationPresent(Inject.class)) {
             for (Parameter parameter : constructor.getParameters()) {
